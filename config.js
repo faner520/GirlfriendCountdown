1a53ab4edbffa3fab36b3a6ad9a497da3bbc095d
Noah Crocker <noah.crocker@snapchat.com>
2016-09-12 14:29:08 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var full_config = {
+    countdown: {
+        date: '2016-10-01 07:39:00',
+        foreground: '#fff',
+        background: 'rgba(0, 0, 0, .15)',
+    },
+    twitter: 'themewizz'
+};
